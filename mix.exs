5532437ab524bb48d99126877f3b281b4f1032f5
Antonio Cabrera <antonio@arryved.com>
2026-05-01 10:51:22 -0600

    chore(deps): bump claudio to 0.5.0

--- mix.exs
+++ mix.exs
@@ -137,7 +137,7 @@ defmodule Normandy.MixProject do
       {:elixir_uuid, "~> 1.2"},
       {:poison, "~> 6.0"},
       {:telemetry, "~> 1.0"},
-      {:claudio, "~> 0.4.0"},
+      {:claudio, "~> 0.5.0"},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:stream_data, "~> 1.1", only: [:dev, :test]},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false}
