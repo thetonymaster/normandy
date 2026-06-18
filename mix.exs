91c024d9fed7e3f6c486b82c76b1c7b92805b667
Antonio Cabrera <antonio@arryved.com>
2026-06-18 12:59:42 -0600

    feat: add Mnesia SessionStore (distributed-ETS, transactional appends)

--- mix.exs
+++ mix.exs
@@ -129,7 +129,7 @@ defmodule Normandy.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :mnesia]
     ]
   end
 
